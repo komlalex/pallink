8f1cd41dca13eac3bfe8b4c628068ffc585f8731
Kunyelewosi Alexander <kunalexander360@gmail.com>
2023-04-30 17:36:04 +0000

    signup page created

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,11 @@
 import "./styles/css/main.css";
-import Navbar from "./components/Navbar";
-import Landing from "./components/Landing"
+//import Navbar from "./components/Navbar";
+import SignUp from "./components/Signup"
 
 function App() {
   return (
     <div>
-      <Landing/>
+      <SignUp/>
     </div>
   );
 }
---
+++ client/src/components/Signup.js
@@ -0,0 +1,84 @@
+import React from 'react';
+import {useFormik} from "formik";
+import * as Yup from "yup";
+
+function Signup() {
+    const formik = useFormik({
+        initialValues: {
+            firstName: "",
+            lastName: "",
+            email: "",
+            password: ""
+        },
+        validationSchema: Yup.object({
+            firstName: Yup.string().max(15, "Must be 15 characters or less").required("Required"),
+            lastName: Yup.string().max(15, "Must be 15 characters or less").required("Required"),
+            email: Yup.string().email("Invalid email").required("Required"),
+            password: Yup.string().max(15, "Must be 15 characters or less").required("Required")
+        }),
+        onSubmit: values => {
+            console.log(values)
+        }
+    })
+  return (
+    <div className='signup'>
+        <h2>Sign Up Form</h2>
+    <form onSubmit={formik.handleSubmit}>
+        <label htmlFor='firstName'>First Name</label>
+        {formik.touched.firstName && formik.errors.firstName ? (
+            <div className="errorMessage" >{formik.errors.firstName}</div>
+        ): null
+        }
+        <input 
+        type='text' 
+        id='firstName' 
+        name="firstName" 
+        onChange={formik.handleChange} 
+        value = {formik.values.firstName}
+        onBlur={formik.handleBlur}/>
+        
+
+        <label htmlFor='lastName'>Last Name</label>
+        {formik.touched.lastName && formik.errors.lastName ? (
+            <div className="errorMessage">{formik.errors.lastName}</div>
+        ): null
+        }
+        <input type='text' 
+        id='lastName' 
+        name="lastName" 
+        onChange={formik.handleChange} 
+        value = {formik.values.lastName}
+        onBlur={formik.handleBlur}/>
+
+        <label htmlFor='email'>Email</label>
+        {formik.touched.email && formik.errors.email ? (
+            <div className="errorMessage">{formik.errors.email}</div>
+        ): null
+        }
+        <input type='email'
+         id='email' 
+         name="email" 
+         onChange={formik.handleChange} 
+         value = {formik.values.email}
+         onBlur={formik.handleBlur}/>
+
+        <label htmlFor='password'>Password</label>
+        {formik.touched.password && formik.errors.password ? (
+            <div className="errorMessage">{formik.errors.password}</div>
+        ): null
+        }
+        <input type='password' 
+        id='password' 
+        name="password" 
+        onChange={formik.handleChange} 
+        value = {formik.values.password}
+        onBlur={formik.handleBlur}/>
+
+
+        <button type="submit">Sign Up</button>
+    </form>
+    </div>
+  )
+}
+
+export default Signup
