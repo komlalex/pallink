3dda71f0f03e6df9b6ac866dbf45e9d499c6ce72
Kunyelewosi Alexander <kunalexander360@gmail.com>
2023-05-01 17:52:42 +0000

    swr configuration established

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,24 @@
 import "./styles/css/main.css";
+import axios from "axios";
+import { SWRConfig } from "swr";
 //import Navbar from "./components/Navbar";
 //import Footer from "./components/Footer";
 //import Landing from "./components/Landing";
 //import Chats from "./components/Chats";
-import People from "./components/People";
 
+
+
+import People from "./components/People";
+axios.defaults.baseURL = "http://localhost:1776";
 function App() {
+  const fetcher  = (url) => axios.get(url).then(res => res.data);
   return (
-    <div>
+    <SWRConfig value={{fetcher}}>
+      <div>
       <People/>
     </div>
+    </SWRConfig>
+    
   );
 }
 
--- client/src/components/People.js
+++ client/src/components/People.js
@@ -1,9 +1,20 @@
 import React from 'react';
-//import useSWR from "swr";
+import useSWR from "swr";
 
 function People() {
+   const {data, error, isLoading} = useSWR("/users");
+
+   if (error) return <div>Failed</div>
+   if (isLoading) return <div>laoding...</div>
+   console.log(data.success, data.message);
   return (
-    <div>People</div>
+    <div>
+        {
+            data.message.map ((user, index) => {
+                return <div key ={index}>{user.firstname}</div>
+            })
+        }
+    </div>
   )
 }
 
